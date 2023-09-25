eb1f241a933def106a344a07e66d2a4061710be7
alisajoangarcia <abgarcia@purdue.edu>
2023-09-25 15:24:35 -0400

    finished home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -6,30 +6,22 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+class PlaylistInfo {
+  String title;
+  String image;
+  int numSongs;
+
+  PlaylistInfo(this.title, this.image, this.numSongs);
+}
+
+
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
-  List<Map<String, String>> cardData = [
-    {
-      'title': 'ish and charlie like to party',
-      'image': 'assets/green_colored_logo.png',
-    },
-    {
-      'title': 'group running playlist',
-      'image': 'assets/blue_colored_logo.png',
-    },
-    {
-      'title': 'trombone tunes',
-      'image': 'assets/red_colored_logo.png',
-    },
-    // Add more data as needed
-  ];
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Tab 1 Content'),
-    Text('Tab 2 Content'),
-    Text('Tab 3 Content'),
-    Text('Tab 4 Content'),
+  List<PlaylistInfo> cardData = [
+    PlaylistInfo('ish and charlie like to party', 'assets/green_colored_logo.png', 20),
+    PlaylistInfo('group running playlist', 'assets/blue_colored_logo.png', 30),
+    PlaylistInfo('trombone tunes', 'assets/red_colored_logo.png', 50),
   ];
 
   void _onItemTapped(int index) {
@@ -40,6 +32,10 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    final textScaleFactor = MediaQuery.of(context).textScaleFactor;
+    final double screenWidth = screenSize.width;
+    final double screenHeight = screenSize.height;
     return Scaffold(
       backgroundColor: MixTapeColors.black,
       appBar: AppBar(
@@ -47,13 +43,13 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(
             fontFamily: 'Montserrat',
             fontWeight: FontWeight.w600,
-            fontSize: 25,
+            fontSize: (25.0 * textScaleFactor),
           ),
         ),
         backgroundColor: MixTapeColors.black,
         automaticallyImplyLeading: false,
         elevation: 0.0,
-        toolbarHeight: 100,
+        toolbarHeight: screenHeight * .13,
         actions: [
           Padding(
             padding: const EdgeInsets.all(10.0),
@@ -61,24 +57,137 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
-      body: ListView.builder(
-        itemCount: cardData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            elevation: 3.0,
-            margin: EdgeInsets.all(8.0),
-            child: ListTile(
-              leading: Image.asset(cardData[index]['image']!),
-              title: Text(cardData[index]['title']!, style: TextStyle(fontSize: 20)),
-              subtitle: Text(cardData[index]['image']!),
-              onTap: () {
-                // Handle card tap here
-                print('Tapped on Card ${cardData[index]['title']}');
-              },
+      body: Column(
+        children: [
+          Container(
+            height: screenHeight * .67,
+            padding: EdgeInsets.fromLTRB(5, 0, 5, 30),
+            child: SingleChildScrollView( // Use SingleChildScrollView instead of ListView
+              child: Container(
+                child: Column(
+                  children: cardData.map((playlist) {
+                    return InkWell(
+                      borderRadius: BorderRadius.circular(12.0),
+                      onTap: () {
+                        print('Tapped on Card ${playlist.title}');
+                      },
+                      child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0), // Adjust the radius as needed
+                          ),
+                          elevation: 3.0,
+                          margin: EdgeInsets.all(15.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12.0),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    height: screenHeight * .2,
+                                    color: MixTapeColors.dark_gray,
+                                    child: Image.asset(playlist.image),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    padding: EdgeInsets.only(top: 10, bottom: 5, left: 10, right: 10),
+                                    height: screenHeight * .2,
+                                    color: MixTapeColors.light_gray,
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          playlist.title,
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            fontSize: (22 * textScaleFactor),
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        FilledButton(
+                                          style: FilledButton.styleFrom(
+                                            backgroundColor: MixTapeColors.dark_gray,
+                                            padding: EdgeInsets.all(0),
+                                            fixedSize: Size(150, 15),
+                                          ),
+                                          onPressed: null,
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                            children: [
+                                              Image.asset('assets/blue_colored_logo.png', width: 25, height: 25),
+                                              Text(
+                                                "with alexfrey2",
+                                                style: TextStyle(
+                                                  fontSize: (10 * textScaleFactor),
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        Card(
+                                          color: MixTapeColors.light_gray,
+                                          elevation: 0.0,
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                "${playlist.numSongs} songs",
+                                                style: TextStyle(
+                                                  fontSize: (12 * textScaleFactor),
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                              Text(
+                                                '${3} hours, ${30} min',
+                                                style: TextStyle(
+                                                  color: Colors.grey[400],
+                                                  fontSize: (12 * textScaleFactor),
+                                                ),
+                                              ),
+                                              Image.asset(
+                                                'assets/spotify/Spotify_Icon_RGB_Green.png',
+                                                height: screenHeight * .03,
+                                                width: screenHeight * .03,
+                                              ),
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
             ),
-          );
-        },
+          ),
+          FloatingActionButton.extended(
+            onPressed: () {
+              // Add your button action here
+            },
+            label: Text(
+                'Create a Playlist',
+                style: TextStyle(
+                  fontSize: textScaleFactor * 20,
+                  fontFamily: "Montserrat",
+                  fontWeight: FontWeight.w600,
+                ),
+            ),
+            icon: Icon(Icons.add),
+            backgroundColor: MixTapeColors.green, // Change the button's color
+          ),
+        ],
       ),
+
       bottomNavigationBar: Theme(
         data: ThemeData(
           canvasColor: MixTapeColors.black, // Set the canvasColor to black
