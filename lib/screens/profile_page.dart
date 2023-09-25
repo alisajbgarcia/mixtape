28264bacbb81e91b88a765cefe0c33ca189d486a
joeypowers <jjpowers@purdue.edu>
2023-09-25 15:34:57 -0400

    Styled profile page

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,48 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:mixtape/screens/home_page.dart';
 
 import '../utilities/colors.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  // int _selectedIndex = 0;
+  //
+  // static List<Widget> _pages = <Widget>[
+  //   HomePage(),
+  //   ProfilePage(),
+  //   ProfilePage(),
+  // ];
+  //
+  // void _onItemTapped(int index) {
+  //   Navigator.of(context as BuildContext).push(
+  //     MaterialPageRoute(builder: (context) => _pages.elementAt(_selectedIndex)),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MixTapeColors.black,
-      appBar: AppBar(
-        title: Text('Profile Page'),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: MixTapeColors.black,
+      //   title: Text('Profile Page'),
+      // ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          // Flexible(
+          //   flex: 1,
+          //   child: Align(
+          //     alignment: Alignment.topRight,
+          //     child: IconButton(
+          //     icon: const Icon(Icons.settings),
+          //     onPressed: () {
+          //       print("settings");
+          //     },
+          //   ),),
+          // ),
           Flexible(
             flex: 1,
             child: Column(
               children: [
-                const Text(
-                  'Profile',
-                  style: TextStyle(
+                Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: const Text(
+                    'Profile',
+                    style: TextStyle(
                       color: Colors.white,
                       fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                    ),
+                  ),
                 ),
                 // Profile picture, using MixTape logo temporarily until Spotify Link
-                Container(
-                    width: 200,
-                    height: 200,
-                    decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          fit: BoxFit.fill,
-                          image: AssetImage('assets/green_colored_logo.png'),
-                        ))),
-                const Text(
-                  'Ish Mistry',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold),
+                Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: Container(
+                      width: MediaQuery.of(context).size.width * .5,
+                      height: MediaQuery.of(context).size.width * .5,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage('assets/green_colored_logo.png'),
+                          ))),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: const Text(
+                    'ish mistry',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               ],
             ),
@@ -50,14 +93,18 @@ class ProfilePage extends StatelessWidget {
           Flexible(
             flex: 1,
             child: Padding(
-              padding: EdgeInsets.only(left: 50, right: 50),
+              padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * .1,
+                  right: MediaQuery.of(context).size.width * .1),
               child: ElevatedButton(
                 style: ButtonStyle(
                   backgroundColor:
                       MaterialStateProperty.all<Color>(Colors.black),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(MixTapeColors.green),
-                  padding: MaterialStateProperty.all(EdgeInsets.only(left: 25, right: 100)),
+                  fixedSize: MaterialStateProperty.all(Size.fromHeight(
+                      MediaQuery.of(context).size.height * .12)),
+                  padding: MaterialStateProperty.all(EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * .05,
+                      right: MediaQuery.of(context).size.width * .05)),
                   shape: MaterialStateProperty.all(
                     RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20.0)),
@@ -74,15 +121,22 @@ class ProfilePage extends StatelessWidget {
                       flex: 1,
                       child: Image.asset(
                         'assets/spotify/Spotify_Icon_RGB_Green.png',
-                        width: 100,
+                        width: 50,
                         height: 100,
                       ),
                     ),
                     Flexible(
                       flex: 5,
-                      child: const Text(
-                        'Logout',
-                        style: TextStyle(fontFamily: 'Montserrat'),
+                      child: Center(
+                        child: const Text(
+                          'Logout',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: MixTapeColors.green,
+                          ),
+                        ),
                       ),
                     ),
                   ],
@@ -92,6 +146,25 @@ class ProfilePage extends StatelessWidget {
           ),
         ],
       ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home),
+      //       label: 'Tab 1',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.business),
+      //       label: 'Tab 2',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.account_circle_rounded),
+      //       label: 'Profile',
+      //     ),
+      //   ],
+      //   currentIndex: _selectedIndex,
+      //   selectedItemColor: Colors.blue,
+      //   onTap: _onItemTapped,
+      // ),
     );
   }
 }
--- lib/utilities/colors.dart
+++ lib/utilities/colors.dart
@@ -6,9 +6,9 @@ import 'package:flutter/material.dart';
  */
 class MixTapeColors {
   //colors must have FF in front when going from hex to Color because Color takes an integer parameter
-  static final Color black = Color(0xFF1E1E1E);
-  static final Color light_gray = Color(0xFF474646);
-  static final Color dark_gray = Color(0xFF343030);
-  static final Color green = Color(0xFF178E41);
-  static final Color mint = Color(0xFF83B899);
+  static const Color black = Color(0xFF1E1E1E);
+  static const Color light_gray = Color(0xFF474646);
+  static const Color dark_gray = Color(0xFF343030);
+  static const Color green = Color(0xFF1DB954);
+  static const Color mint = Color(0xFF83B899);
 }
