f3b8eb3b373243a55d99a106cb759e2c0b4efd43
alisajoangarcia <abgarcia@purdue.edu>
2023-09-25 23:22:28 -0400

    created playlist creation screen, added new assets

---
+++ lib/screens/playlist_creation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:mixtape/utilities/colors.dart';
+
+class PlaylistCreationScreen extends StatefulWidget {
+  const PlaylistCreationScreen({super.key});
+
+  @override
+  State<PlaylistCreationScreen> createState() => _PlaylistCreationScreenState();
+}
+
+class _PlaylistCreationScreenState extends State<PlaylistCreationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
