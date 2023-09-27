a389ae0ede3e347a450ca3ae0430aa7f4bdf8ba4
Andrew Thomae <andrewthomae@Andrews-MacBook-Air-3.local>
2023-09-27 10:27:07 -0400

    Little more Friends screen function, will make add friends button later today

--- lib/screens/friends_page.dart
+++ lib/screens/friends_page.dart
@@ -3,11 +3,22 @@ import 'package:flutter/material.dart';
 import '../utilities/navbar_pages.dart';
 import '../widgets/navbar.dart';
 
+import 'package:flutter/material.dart';
+import 'package:mixtape/utilities/colors.dart';
+import 'package:mixtape/widgets/navbar.dart';
+
 class FriendsPage extends StatefulWidget {
   const FriendsPage({Key? key}) : super(key: key);
 
   @override
   State<FriendsPage> createState() => _FriendsPageState();
+}
+
+class FriendsInfo {
+  String username;
+  String image;
+
+  FriendsInfo(this.username, this.image);
 }
 
 class _FriendsPageState extends State<FriendsPage> {
@@ -24,11 +35,116 @@ class _FriendsPageState extends State<FriendsPage> {
     );
   }
 
+  List<FriendsInfo> cardData = [
+    FriendsInfo('zestythomae', 'assets/green_colored_logo.png'),
+    FriendsInfo('alisagarcia', 'assets/green_colored_logo.png'),
+    FriendsInfo('joeypowers', 'assets/green_colored_logo.png'),
+    FriendsInfo('charliesale', 'assets/green_colored_logo.png'),
+    FriendsInfo('alexfrey', 'assets/green_colored_logo.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    final textScaleFactor = MediaQuery.of(context).textScaleFactor;
+    final double screenWidth = screenSize.width;
+    final double screenHeight = screenSize.height;
     return Scaffold(
-      body: Center(
-        child: Text("Friends Page placeholder"),
+      backgroundColor: MixTapeColors.black,
+      appBar: AppBar(
+        title: Text('Your Friends',
+          style: TextStyle(
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.w600,
+            fontSize: (25.0 * textScaleFactor),
+          ),
+        ),
+        backgroundColor: MixTapeColors.black,
+        automaticallyImplyLeading: false,
+        elevation: 0.0,
+        toolbarHeight: screenHeight * .13,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Image.asset('assets/blue_colored_logo.png'),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: screenHeight * .67,
+            padding: EdgeInsets.fromLTRB(5, 0, 5, 30),
+            child: SingleChildScrollView( // Use SingleChildScrollView instead of ListView
+              child: Container(
+                child: Column(
+                  children: cardData.map((friend) {
+                    return InkWell(
+                      borderRadius: BorderRadius.circular(12.0),
+                      onTap: () {
+                        print('Tapped on Friend ${friend.username}');
+                      },
+                      child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0), // Adjust the radius as needed
+                          ),
+                          elevation: 3.0,
+                          margin: EdgeInsets.all(15.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12.0),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    height: screenHeight * .07,
+                                    color: MixTapeColors.dark_gray,
+                                    child: Image.asset(friend.image),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    padding: EdgeInsets.only(top: 10, bottom: 5, left: 10, right: 10),
+                                    height: screenHeight * .07,
+                                    color: MixTapeColors.light_gray,
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Align(
+                                          alignment: Alignment.center,
+                                          child: Text(
+                                            friend.username,
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              fontSize: (22 * textScaleFactor),
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                        Card(
+                                          color: MixTapeColors.light_gray,
+                                          elevation: 0.0,
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: NavBar(
         currentIndex: _selectedIndex,
