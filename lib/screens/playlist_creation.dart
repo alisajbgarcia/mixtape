9d6b2e1d298f8a13826bae64482858df1b777f6d
alisajoangarcia <abgarcia@purdue.edu>
2023-09-27 10:54:14 -0400

    initial implementation for playlist creation screen

--- lib/screens/playlist_creation.dart
+++ lib/screens/playlist_creation.dart
@@ -19,11 +19,44 @@ class _PlaylistCreationScreenState extends State<PlaylistCreationScreen> {
     return Container(
       width: screenWidth,
       height: screenHeight,
-      decoration: BoxDecoration(
-        color: MixTapeColors.black,
+      color: MixTapeColors.black,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(5, 40, 5, 0),
+        child: Container(
+          width: screenWidth,
+          height: screenHeight,
+          decoration: BoxDecoration(
+            color: MixTapeColors.black,
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/mixtape_image.png',
+              ),
+              alignment: Alignment.topCenter,
+              scale: 1.04,
+            ),
+          ),
+          child: Column(
+            children: [
+              Text(
+                "This is a custom font",
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                "This is a custom font",
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+
+          ),
+
+        ),
       ),
-
-
     );
   }
 }
