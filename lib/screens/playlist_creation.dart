e600ea84e119df81ac75b43373fae27e9b44bb93
ishmistry <imistry@purdue.edu>
2023-10-02 15:12:12 -0400

    Removed my rendition of tape creation, joey is making. need to refactor branch name

--- lib/screens/playlist_creation.dart
+++ lib/screens/playlist_creation.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mixtape/utilities/colors.dart';
-import 'package:mixtape/screens/tape_creation.dart';
+import 'package:mixtape/screens/search_page.dart';
 
 class PlaylistCreationScreen extends StatefulWidget {
   const PlaylistCreationScreen({super.key});
@@ -143,7 +143,7 @@ class _PlaylistCreationScreenState extends State<PlaylistCreationScreen> {
                   print("send invitation");
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => TapeCreationPage()),
+                    MaterialPageRoute(builder: (context) => SearchPage()),
                   );
                 },
                 label: Padding(
--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mixtape/utilities/colors.dart';
-import 'package:mixtape/screens/tape_creation.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
