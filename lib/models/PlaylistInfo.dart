4d37d7708277e3f45265a534b348948d4cd2066e
alisajoangarcia <abgarcia@purdue.edu>
2023-10-04 15:09:52 -0400

    made shii more modular

---
+++ lib/models/PlaylistInfo.dart
@@ -0,0 +1,11 @@
+class PlaylistInfo {
+  String title;
+  String image;
+  int numSongs;
+  String friend;
+  String friendProfile;
+  int hours;
+  int minutes;
+
+  PlaylistInfo(this.title, this.image, this.numSongs, this.friend, this.friendProfile, this.hours, this.minutes);
+}
