fb7e9bb67085acf86c4876ba06ff4736eaa2e77f
alisajoangarcia <abgarcia@purdue.edu>
2023-10-25 18:36:50 -0400

    added alert dialog upon clicking add photo icon

--- lib/widgets/image_upload.dart
+++ lib/widgets/image_upload.dart
@@ -16,6 +16,62 @@ class _ImageUploadState extends State<ImageUpload> {
     final double screenWidth = screenSize.width;
     final double screenHeight = screenSize.height;
 
-    return const Placeholder();
+    return AlertDialog(
+      backgroundColor: MixTapeColors.black,
+      title: Text(
+        'Invite a friend',
+        style: TextStyle(
+          color: Colors.white,
+          fontFamily: 'Montserrat',
+          fontWeight: FontWeight.w600,
+          fontSize: textScaleFactor * 20,
+        ),
+      ),
+      content: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: screenWidth * 0.7,
+              height: screenHeight * 0.12,
+              child: Center(
+                child: Text("Hello there", style: TextStyle(
+                  color: Colors.white,
+                )),
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: screenWidth * .2,
+            height: screenHeight * .05,
+            child: FloatingActionButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              backgroundColor: MixTapeColors.green,
+              onPressed: () {
+                print('image upload dialog');
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Close',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  fontSize: textScaleFactor * 15,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
