abe7ed70d26ac6b480ca74e00e173c663f79c2d7
alisajoangarcia <abgarcia@purdue.edu>
2023-10-30 16:50:43 -0400

    reverted some stuff

--- lib/screens/playlist_screen.dart
+++ lib/screens/playlist_screen.dart
@@ -164,7 +164,7 @@ class _PlaylistScreenState extends State<PlaylistScreen> {
                   right: 0,
                   top: screenHeight * .25, // Adjust the top position as needed
                   //child: Text('here', style: TextStyle(color: Colors.white)),
-                  child: Image.network(widget.playlist.initiator.profilePicURL, width: screenWidth * .01, height: screenWidth * .01),
+                  child: Image.network(exampleProfile.profilePicURL, width: screenWidth * .01, height: screenWidth * .01),
                 ),
               ],
             ),
