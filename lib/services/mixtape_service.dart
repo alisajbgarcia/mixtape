c59ec885fa5e210d27c79211a1f00e133ccdf375
ishmistry <imistry@purdue.edu>
2023-11-02 15:52:58 -0400

    Searching in playlists for tapes works

--- lib/services/mixtape_service.dart
+++ lib/services/mixtape_service.dart
@@ -12,6 +12,22 @@ class MixtapeService extends AbstractService {
     return getMany("/api/v1/profile/me/playlist/$playlistId/mixtape", Mixtape.fromJson);
   }
 
+  Future<List<Mixtape>> getMixtapesbyTapeForPlaylistCurrentUser(String playlistId, String query) async {
+    return getMany("/api/v1/profile/me/playlist/$playlistId/mixtape?title=$query", Mixtape.fromJson);
+  }
+
+  Future<List<Mixtape>> getMixtapesbySongForPlaylistCurrentUser(String playlistId, String query) async {
+    return getMany("/api/v1/profile/me/playlist/$playlistId/mixtape?songName=$query", Mixtape.fromJson);
+  }
+
+  Future<List<Mixtape>> getMixtapesbyArtistForPlaylistCurrentUser(String playlistId, String query) async {
+    return getMany("/api/v1/profile/me/playlist/$playlistId/mixtape?artistName=$query", Mixtape.fromJson);
+  }
+
+  Future<List<Mixtape>> getMixtapesbyAlbumForPlaylistCurrentUser(String playlistId, String query) async {
+    return getMany("/api/v1/profile/me/playlist/$playlistId/mixtape?albumName=$query", Mixtape.fromJson);
+  }
+
   Future<Mixtape> getMixtapeInPlaylistCurrentUser(String playlistId, String mixtapeId) async {
     return get("/api/v1/profile/me/playlist/$playlistId/mixtape/$mixtapeId", Mixtape.fromJson);
   }
